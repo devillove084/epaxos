53600e8cf594cf93f1e6481a6fe17fad009c7832
devillove084 <786537003@qq.com>
2021-07-28 14:36:08 +0800

    reasonable change 2

--- server/build.rs
+++ server/build.rs
@@ -3,7 +3,7 @@ use protoc_grpcio;
 
 fn main() {
     let proto_root = ".";
-    let output = "src/quorum";
+    let output = "src/classic";
     info!("cargo:rerun-if-changed={}", proto_root);
     protoc_grpcio::compile_grpc_protos(&["epaxos.proto"], &[proto_root], &output, None)
         .expect("Failed to compile gRPC definitions!");
